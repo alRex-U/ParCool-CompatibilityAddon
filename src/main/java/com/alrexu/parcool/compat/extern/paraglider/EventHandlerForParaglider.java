3d8afa941ef2c687a49cee2259f5d3bf827c5a6d
alRex-U <83890990+alRex-U@users.noreply.github.com>
2024-12-30 18:54:42 +0900

    support Paraglider

--- src/main/java/com/alrexu/parcool/compat/extern/paraglider/EventHandlerForParaglider.java
+++ src/main/java/com/alrexu/parcool/compat/extern/paraglider/EventHandlerForParaglider.java
@@ -1,23 +1,31 @@
-package com.alrexu.parcool.compat.extern.carryon;
+package com.alrexu.parcool.compat.extern.paraglider;
 
 import com.alrex.parcool.api.unstable.action.ParCoolActionEvent;
 import com.alrex.parcool.api.unstable.animation.ParCoolAnimationInfoEvent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import tictim.paraglider.item.ParagliderItem;
 
-public class EventHandlerForCarryOn {
+public class EventHandlerForParaglider {
     @OnlyIn(Dist.CLIENT)
     @SubscribeEvent
     public static void onUpdateParCoolAnimInfo(ParCoolAnimationInfoEvent event) {
-        if (CarryOnManager.isCarrying(event.getPlayer())) {
+        if (ParagliderItem.isItemParagliding(event.getPlayer().getMainHandItem())) {
             event.getOption().cancelAnimation();
         }
     }
 
     @SubscribeEvent
     public static void onTryToStart(ParCoolActionEvent.TryToStartEvent event) {
-        if (CarryOnManager.isCarrying(event.getPlayer())) {
+        if (ParagliderItem.isItemParagliding(event.getPlayer().getMainHandItem())) {
+            event.setCanceled(true);
+        }
+    }
+
+    @SubscribeEvent
+    public static void onTryToContinue(ParCoolActionEvent.TryToContinueEvent event) {
+        if (ParagliderItem.isItemParagliding(event.getPlayer().getMainHandItem())) {
             event.setCanceled(true);
         }
     }
